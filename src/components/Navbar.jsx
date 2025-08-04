241a58fad5a6fd7ed345e341788327d1b9b025b3
Franco Urra <kofran.cl@gmail.com>
2025-08-04 19:18:04 -0400

    Se- arreglan las mediaqueris  logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,6 @@ function Navbar() {
           src="/clap.png"
           alt="Logo Clap"
           className="navbar-logo"
-          style={{ height: '70px', width: '70px', objectFit: 'contain', marginLeft: 0 }}
         />
         <h1 className="navbar-title" style={{ margin: 0, flex: 1, textAlign: 'center', paddingLeft: 0 }}>
           Buscador de Películas y Series
